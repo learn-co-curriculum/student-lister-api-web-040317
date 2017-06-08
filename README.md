c08851787da8cec986257d1f9820d90c558db191
ipc103 <iancandysemail@gmail.com>
2017-06-08 15:59:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+### The Plan
+
+!. When someone makes a POST request to /api/v1/auth - we want to look at their username and their password
+find the account and authenticate them
+@. If everything is good, we should send back a special token that stores some info about them
---
+++ app/controllers/api/v1/students_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::StudentsController < ApplicationController
+
+  def index
+    students = Student.all
+    render json: students
+  end
+
+  def create
+    student = Student.create(student_params)
+    render json: student
+  end
+
+  def update
+    student = Student.find(params[:id])
+    student.update(student_params)
+    render json: student
+  end
+
+  def destroy
+    student = Student.find(params[:id])
+    student.destroy
+    render json: student
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name)
+  end
+end
